c3ff6b9e15daad947ceee66b02528af1efbc0d20
lakshay402 <lakshay402@gmail.com>
2021-06-29 23:03:27 +0530

    First cucumber Rest code

---
+++ src/test/java/stepDefinitions/StepDefinitions.java
@@ -0,0 +1,52 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+	
+	@Given("Starting Test Case {string}")
+	public void starting_test_case(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Create Repo Payload name {string} amd description {string}")
+	public void create_repo_payload_name_amd_description(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User calls Create API {string} Post API Call")
+	public void user_calls_create_api_post_api_call(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("API Call got Successful with Status Code {int}")
+	public void api_call_got_successful_with_status_code(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Verify Repository {string} is {string}")
+	public void verify_repository_is(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Ending TestCase")
+	public void ending_test_case() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User calls Delete API {string} Delete Call")
+	public void user_calls_delete_api_delete_call(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
